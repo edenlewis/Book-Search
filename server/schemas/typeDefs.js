1501752a9e10b9e1e3e5b59abe609e9a4a2e89c4
edenlewis <edenlewis80@gmail.com>
2023-12-10 15:57:56 -0700

    finsihed server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,9 +14,28 @@ type Book{
     image: String
     link: String
 }
+
+input BookData{
+    bookId: String
+    authors:[String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
 type Auth{
     token: ID!
     user:[User]
+}
+type Query{
+    me: User
+}
+
+type Mutation{
+    addUser(username: String!, email: String!, password: Sting!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: BookData!):User
+    removeBook(bookId: ID!): User 
 }
 `
 module.exports = typeDefs;
